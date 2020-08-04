0d2f8e5325db8efe3bdebd3ba082f481568e1179
dnsgusttavo <gustavo.maltez@hotmail.com>
2020-08-04 20:35:43 -0300

    Added switch on router page and added 404 page

--- web/src/pages/routes.tsx
+++ web/src/pages/routes.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Landing from './Landing';
 import TeacherList from './TeacherList';
 import TeacherForm from './TeacherFrom';
+import NotFound from './NotFound';
 
 const Routes = () => {
     return(
         <BrowserRouter>
-            <Route path="/" exact component={Landing}/>
-            <Route path="/study" exact component={TeacherList}/>
-            <Route path="/give-classes" exact component={TeacherForm}/>
+            <Switch>
+                <Route path="/" exact component={Landing}/>
+                <Route path="/study" exact component={TeacherList}/>
+                <Route path="/give-classes" exact component={TeacherForm}/>
+                ]<Route component={NotFound}/>
+            </Switch>
         </BrowserRouter>
     )
 }
