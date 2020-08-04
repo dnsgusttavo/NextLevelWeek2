34a4c7d4f4d4340f5603e023386f641924530283
dnsgusttavo <gustavo.maltez@hotmail.com>
2020-08-04 20:58:39 -0300

    Added 404 Page Not Found

--- web/src/pages/NotFound/index.tsx
+++ web/src/pages/NotFound/index.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/images/landing.svg'
+
 import './styles.css';
 
 const NotFound = () => {
     return(
-        <h1>Not Found!</h1>
+        <main id="nf-container">
+            <h1>Ops! Página não encontrada</h1>
+            <img src={logo} alt="Logo"/>
+            <Link to="/" id="back">
+                <p>Conheça os Proffys ou se torne um!</p>      
+            </Link>
+        </main>
     )
 }
 
