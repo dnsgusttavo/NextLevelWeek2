98f9cc2251004143a55395f758a96c901025dc51
dnsgusttavo <gustavo.maltez@hotmail.com>
2020-08-04 11:21:48 -0300

    Added knex and sqlite3

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,11 @@
-console.log("Hello World!2");
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.post('/users', (req, res) => {
+    return res.json({msg: "Hello World!s"});
+})
+
+app.listen(3333);
