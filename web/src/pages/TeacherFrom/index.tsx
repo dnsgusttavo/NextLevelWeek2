a19134465f9467d3b86a4ca9cdaa992827089ef3
dnsgusttavo <gustavo.maltez@hotmail.com>
2020-08-06 10:15:53 -0300

    Added Select and Textarea components

--- web/src/pages/TeacherFrom/index.tsx
+++ web/src/pages/TeacherFrom/index.tsx
@@ -5,6 +5,8 @@ import PageHeader from '../../components/PageHeader';
 import warningIcon from '../../assets/images/icons/warning.svg';
 import Input from '../../components/Input';
 import './styles.css';
+import Textarea from '../../components/Textarea';
+import Select from '../../components/Select';
 
 const TeacherForm = () => {
     return(
@@ -19,11 +21,25 @@ const TeacherForm = () => {
                     <Input name="name" label="Nome completo"/>
                     <Input name="avatar" label="Avatar"/>
                     <Input name="whatsapp" label="WhatsApp"/>
+                    <Textarea name="bio" label="Biografia"/>
                </fieldset>
 
                <fieldset>
                     <legend>Sobre a aula</legend>
-                    <Input name="subject" label="Matéria"/>
+                    <Select name="subject" label="Matéria" options={[
+                        {value: 'Matemática', label: 'Matemática'},
+                        {value: 'Física', label: 'Física'},
+                        {value: 'Artes', label: 'Artes'},
+                        {value: 'Biologia', label: 'Biologia'},
+                        {value: 'Ciências', label: 'Ciências'},
+                        {value: 'Educação Física', label: 'Educação Física'},
+                        {value: 'Geografia', label: 'Geografia'},
+                        {value: 'História', label: 'História'},
+                        {value: 'Português', label: 'Português'},
+                        {value: 'Química', label: 'Química'},
+                        {value: 'Redação', label: 'Redação'},
+                        {value: 'Literatura', label: 'Literatura'},
+                    ]}/>
                     <Input name="cost" label="Custo da sua hora por aula"/>
                </fieldset>
 
